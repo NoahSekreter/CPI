3a3ba9fc83faacf342698c7d5a37284b85f922b6
NoahSekreter <nsekreter@gmail.com>
2017-08-31 20:43:59 -0400

    Routine update

--- CastlePropertyInspections/script.js
+++ CastlePropertyInspections/script.js
@@ -40,3 +40,27 @@ $('a[href*="#"]')
       }
   });
 
+var modal = document.getElementById('popUp');
+var btn = document.getElementById("inspection-button");
+var span = document.getElementsByClassName("close")[0];
+var body = document.body;
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+    body.style.overflow = "hidden";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+    body.style.overflow = "auto";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        body.style.overflow = "auto";
+    }
+}
